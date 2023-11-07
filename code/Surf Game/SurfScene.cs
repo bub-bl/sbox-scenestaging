b07fcd645af1ef11d0fd2c26a299ce884a6ed3e5
cr4yz <jacob.ee92@gmail.com>
2023-11-06 22:10:57 -0600

    Create SurfScene.cs

---
+++ code/Surf Game/SurfScene.cs
@@ -0,0 +1,14 @@
+﻿
+using Sandbox;
+
+internal class SurfScene : BaseComponent
+{
+
+	public override void OnEnabled()
+	{
+		base.OnEnabled();
+
+		Game.TickRate = 64; // this would be good
+	}
+
+}
